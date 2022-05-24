d5917830afa5a2de4bde5123e4609d8e207ddc61
jacikot <jana.toljaga725@yahoo.com>
2022-05-24 16:31:28 +0200

    PGP Encryptor added

---
+++ ZPproject/src/etf/openpgp/pd180205dtj180023d/PGPEncryptor.java
@@ -0,0 +1,41 @@
+package etf.openpgp.pd180205dtj180023d;
+
+import org.bouncycastle.bcpg.ArmoredOutputStream;
+import org.bouncycastle.bcpg.HashAlgorithmTags;
+import org.bouncycastle.openpgp.*;
+import org.bouncycastle.openpgp.operator.PBESecretKeyDecryptor;
+import org.bouncycastle.openpgp.operator.PGPContentSignerBuilder;
+import org.bouncycastle.openpgp.operator.PGPDataEncryptorBuilder;
+import org.bouncycastle.openpgp.operator.bc.*;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.security.SecureRandom;
+import java.util.List;
+
+
+public class PGPEncryptor {
+
+    public enum SymetricKeyAlgorithm{
+        CAST5,TRIPLEDES;
+        private int map(){
+            switch (this){
+                case CAST5: return PGPEncryptedData.CAST5;
+                case TRIPLEDES: return PGPEncryptedData.TRIPLE_DES;
+                default: return -1;
+            }
+        }
+    }
+
+    public static OutputStream configureEncryption(SymetricKeyAlgorithm algorythm, List<PGPPublicKey> publicKeys, OutputStream stream) throws IOException, PGPException {
+        BcPGPDataEncryptorBuilder builder=new BcPGPDataEncryptorBuilder(algorythm.map());
+        builder.setSecureRandom(new SecureRandom());
+        builder.setWithIntegrityPacket(true);
+
+        PGPEncryptedDataGenerator generator=new PGPEncryptedDataGenerator(builder);
+        publicKeys.forEach(publicKey -> {
+            generator.addMethod(new BcPublicKeyKeyEncryptionMethodGenerator(publicKey));
+        });
+        return generator.open(stream,PGPProtocol.BUFFER_SIZE);
+    }
+}
---
+++ ZPproject/src/etf/openpgp/pd180205dtj180023d/PGPProtocol.java
@@ -0,0 +1,47 @@
+package etf.openpgp.pd180205dtj180023d;
+
+import org.bouncycastle.openpgp.PGPPublicKey;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.security.PublicKey;
+import java.util.List;
+
+public class PGPProtocol {
+    public static final long BUFFER_SIZE=1<<16;
+
+    public enum PGPOptions {
+        AUTENTICATION, ENCRYPTION, COMPRESSION, COMPATIBILITY
+    }
+
+    //add params
+    public static List<PGPPublicKey> getPublicKeys(){
+        return null;
+    }
+
+    public static void encrypt(String inputFile, PGPEncryptor.SymetricKeyAlgorithm algorythm, List<PGPOptions> options){
+        try{
+            OutputStream output=new FileOutputStream(new File(inputFile+".pgp"));
+            List<PGPPublicKey> publicKeys=getPublicKeys();
+
+            if(options.contains(PGPOptions.COMPATIBILITY)){
+                //PGPEncryptor.configureEncryption(algorythm,)
+            }
+            if(options.contains(PGPOptions.ENCRYPTION)){
+                output=PGPEncryptor.configureEncryption(algorythm,publicKeys,output);
+            }
+            if(options.contains(PGPOptions.COMPRESSION)){
+                //PGPEncryptor.configureEncryption(algorythm,)
+            }
+            if(options.contains(PGPOptions.AUTENTICATION)){
+                //PGPEncryptor.configureEncryption(algorythm,)
+            }
+
+        }
+        catch(Exception e){
+
+        }
+
+    }
+}
