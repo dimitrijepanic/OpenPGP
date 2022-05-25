ebacc1d39cad918f1c3c6371bc48211909a38519
jacikot <jana.toljaga725@yahoo.com>
2022-05-25 12:02:44 +0200

    Added FileDialog into EncryptionDialog

--- ZPproject/src/etf/openpgp/pd180205dtj180023d/EncryptionDialog.java
+++ ZPproject/src/etf/openpgp/pd180205dtj180023d/EncryptionDialog.java
@@ -7,6 +7,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class EncryptionDialog extends Dialog {
+
+    //encryption parameters
+    private PGPEncryptor.SymetricKeyAlgorithm symetricKeyAlgorithm;
+    private final List<PGPProtocol.PGPOptions> options=new ArrayList<>();
+    private String filename;
+
+    //navigation
+    private final java.util.List<NavigationPanel> panels=new ArrayList<>();
+    private final Panel content =new Panel();
+    private Button next;
+    private int index=0;
+
+    //UI components
+    private Checkbox autentication;
+    private Checkbox compression;
+    private Checkbox encryption;
+    private Checkbox compatibility;
+    private FileDialog fd;
+
     public EncryptionDialog(Frame owner) {
         super(owner,"Encryption", true);
         addWindowListener(new WindowAdapter() {
@@ -21,20 +40,17 @@ public class EncryptionDialog extends Dialog {
         fillScreen();
     }
 
-    private PGPEncryptor.SymetricKeyAlgorithm symetricKeyAlgorithm;
-    private List<PGPProtocol.PGPOptions> options=new ArrayList<>();
-
-
-    private java.util.List<NavigationPanel> panels=new ArrayList<>();
-    private Panel content =new Panel();
-    private Button next;
-    private int index=0;
     private void reset() {
         symetricKeyAlgorithm=PGPEncryptor.SymetricKeyAlgorithm.CAST5;
         options.clear();
         panels.forEach(p->p.setVisible(false));
         panels.get(0).setVisible(true);
+        autentication.setState(false);
+        compression.setState(false);
+        encryption.setState(false);
+        compatibility.setState(false);
         index=0;
+        filename=null;
     }
 
     private void fillScreen(){
@@ -42,7 +58,7 @@ public class EncryptionDialog extends Dialog {
         setLayout(new BorderLayout());
         content.setLayout(new CardLayout());
         panels.add(new InitialPanel());
-        panels.add(new AutenticationPanel());
+        panels.add(new AuthenticationPanel());
         panels.add(new EncryptionPanel());
         content.add(panels.get(0));
         content.add(panels.get(1));
@@ -58,32 +74,16 @@ public class EncryptionDialog extends Dialog {
 
     }
 
+    // Dialog card layout pannels
 
-    private void navigate(){
-        panels.forEach(p->p.setVisible(false));
-        NavigationPanel nextPanel=panels.get(index).nextPanel();
-        if(nextPanel!=null){
-            nextPanel.setVisible(true);
-        }
-        else {
-            //pokreni enkripciju
-            setVisible(false);
-            reset();
-        }
-    }
-
-    private Checkbox autentication;
-    private Checkbox compression;
-    private Checkbox encryption;
-    private Checkbox compatibility;
-
-    private abstract class NavigationPanel extends Panel{
+    private abstract static class NavigationPanel extends Panel{
         public abstract NavigationPanel nextPanel();
     }
+
     private class InitialPanel extends NavigationPanel{
         public InitialPanel() {
-            setLayout(new GridLayout(3,2));
-            Label label=new Label("Choose feature: ");
+            setLayout(new GridLayout(4,2));
+            Label label=new Label("Choose options: ");
             Label labele=new Label();
             add(label);
             add(labele);
@@ -97,13 +97,26 @@ public class EncryptionDialog extends Dialog {
             encryption.addItemListener(e->{
                 changeNextLabel();
             });
+            Label loadedFile=new Label();
+            Button filebutton=new Button("Choose file to encrypt");
+            filebutton.addActionListener(b->{
+                if(fd==null) {
+                    fd=new FileDialog(EncryptionDialog.this);
+                }
+                fd.setVisible(true);
+                filename=fd.getDirectory()+"\\"+fd.getFile();
+                loadedFile.setText(fd.getFile());
+            });
+
             add(autentication);
             add(compression);
             add(encryption);
             add(compatibility);
+            add(filebutton);
+            add(loadedFile);
         }
 
-        private void getOptions(){
+        private void setSelectedOptions(){
             if(autentication.getState()) options.add(PGPProtocol.PGPOptions.AUTENTICATION);
             if(encryption.getState()) options.add(PGPProtocol.PGPOptions.ENCRYPTION);
             if(compression.getState()) options.add(PGPProtocol.PGPOptions.COMPRESSION);
@@ -112,13 +125,12 @@ public class EncryptionDialog extends Dialog {
 
         @Override
         public NavigationPanel nextPanel() {
-            getOptions();
+            setSelectedOptions();
             if(autentication.getState()) {
                 index++;
                 autentication.setState(false);
                 changeNextLabel();
                 return panels.get(1);
-
             };
             if(encryption.getState()) {
                 index+=2;
@@ -130,14 +142,13 @@ public class EncryptionDialog extends Dialog {
         }
     }
 
-
-
     private class EncryptionPanel extends NavigationPanel{
-        private Checkbox cast5;
-        private Checkbox trides;
+        private final Checkbox cast5;
+        private final Checkbox trides;
+
         public EncryptionPanel() {
             setLayout(new GridLayout(2,2));
-            Label l=new Label("Select symetric algorythm:");
+            Label l=new Label("Select symmetric algorithm:");
             Label labele=new Label();
             add(l);
             add(labele);
@@ -155,13 +166,8 @@ public class EncryptionDialog extends Dialog {
         }
     }
 
-    private void changeNextLabel(){
-        if(autentication.getState()||encryption.getState())next.setLabel("next");
-        else next.setLabel("finish");
-    }
-
-    private class AutenticationPanel extends NavigationPanel{
-        public AutenticationPanel() {
+    private class AuthenticationPanel extends NavigationPanel{
+        public AuthenticationPanel() {
             Label l=new Label("Autentication");
             add(l);
         }
@@ -176,6 +182,27 @@ public class EncryptionDialog extends Dialog {
             }
             else return null;
         }
+    }
+
+
+    // Navigation
+
+    private void navigate(){
+        panels.forEach(p->p.setVisible(false));
+        NavigationPanel nextPanel=panels.get(index).nextPanel();
+        if(nextPanel!=null){
+            nextPanel.setVisible(true);
+        }
+        else {
+            //pokreni enkripciju
+            setVisible(false);
+            reset();
+        }
+    }
+
+    private void changeNextLabel(){
+        if(autentication.getState()||encryption.getState())next.setLabel("next");
+        else next.setLabel("finish");
     }
 
 }
